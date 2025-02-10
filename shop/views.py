bac37f72148cecee59be9baf2430c085c604deeb
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-10 16:52:26 +0200

    285. Отображение названий курсов на веб странице

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse  # импорт для функции index(request)
+# from . import models
+from .models import Course
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello from the Shop app")
+    courses = Course.objects.all()
+    # return HttpResponse("Hello from the Shop app")
+    # return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
+    return HttpResponse(courses)
