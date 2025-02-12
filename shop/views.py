8f9430cbb3f5056961084105f7f9fa8c340e35b9
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-12 13:05:23 +0200

    296. Отображение страницы 404 если курса нет в базе

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse  # импорт для функции index(request)
+from django.shortcuts import render, get_object_or_404
+# импорт для функции index(request)
+from django.http import HttpResponse, Http404
 # from . import models
 from .models import Course
 # Create your views here.
@@ -10,8 +11,17 @@ def index(request):
     # return HttpResponse("Hello from the Shop app")
     # return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
     # return HttpResponse(courses)
-    return render(request, 'courses.html',{'courses':courses})
+    return render(request, 'courses.html', {'courses': courses})
+
 
 def single_course(request, course_id):
-    course = Course.objects.get(pk=course_id)
-    return render(request, 'single_course.html',{'course': course})
+    # OPTION 1
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'single_course.html', {'course': course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+
+    # OPTION 2
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'single_course.html', {'course': course})
