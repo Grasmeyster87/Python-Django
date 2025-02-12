ada27eb0b7334ce52d15e00d143e7974016c21d1
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-12 16:05:41 +0200

    300. Перенос шаблонов в общую папку

--- shop/views.py
+++ shop/views.py
@@ -11,17 +11,17 @@ def index(request):
     # return HttpResponse("Hello from the Shop app")
     # return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
     # return HttpResponse(courses)
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, 'shop/courses.html', {'courses': courses})
 
 
 def single_course(request, course_id):
     # OPTION 1
     # try:
     #     course = Course.objects.get(pk=course_id)
-    #     return render(request, 'single_course.html', {'course': course})
+    #     return render(request, 'shop/single_course.html', {'course': course})
     # except Course.DoesNotExist:
     #     raise Http404()
 
     # OPTION 2
     course = get_object_or_404(Course, pk=course_id)
-    return render(request, 'single_course.html', {'course': course})
+    return render(request, 'shop/single_course.html', {'course': course})
