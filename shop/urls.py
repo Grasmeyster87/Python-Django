586b94a54f8a6a5bb6f1e37ac39cced5f75a8416
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-12 12:01:08 +0200

    294. Создание шаблона для страницы курса

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views  # импорт из той папки где находится файл urls
 
 urlpatterns = [
-    path('', views.index, name='index')  # импорт функции для маршрута
+    path('', views.index, name='index'),  # импорт функции для маршрута
     path('<int:course_id>', views.single_course, name='single_course')
 ]
 # '' главный путь маршрута
