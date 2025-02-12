0f3e05bb9bc0aad1ca8b977ed77b93f48dde95f0
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-12 15:26:28 +0200

    299. Роутинг с использований имен приложений и маршрутов

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views  # импорт из той папки где находится файл urls
 
+app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),  # импорт функции для маршрута
     path('<int:course_id>', views.single_course, name='single_course')
+    # path('course/<int:course_id>', views.single_course, name='single_course')  # http://127.0.0.1:8000/shop/course/2
 ]
 # '' главный путь маршрута
