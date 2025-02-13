988a49cdf1b060d897ab7ab578c72d1622ff805e
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-13 10:03:15 +0200

    303. Создание моделей для приложения api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+# from django.db import models
+from tastypie.resources import ModelResource
+from shop.models import Category, Course
 
 # Create your models here.
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.object.all()
+        resource_name = 'categories'
+        allowed_methods = ['get']
+    
+class CourseResource(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resource_name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
