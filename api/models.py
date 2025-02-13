b2a690cd6459d43e5dd65afab3c16649fd45d56e
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-02-13 14:04:47 +0200

    308. Настройка аутентификации и авторизации для API

--- api/models.py
+++ api/models.py
@@ -1,17 +1,23 @@
 # from django.db import models
 from tastypie.resources import ModelResource
 from shop.models import Category, Course
+from tastypie.authorization import Authorization
+from .authentication import CustomAuthentication
 
 # Create your models here.
+
 
 class CategoryResource(ModelResource):
     class Meta:
         queryset = Category.objects.all()
         resource_name = 'categories'
         allowed_methods = ['get']
-    
+
+
 class CourseResource(ModelResource):
     class Meta:
         queryset = Course.objects.all()
         resource_name = 'courses'
         allowed_methods = ['get', 'post', 'delete']
+        authentication = CustomAuthentication()
+        authorization = Authorization()
